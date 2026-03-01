43e01aaeca58fb76ab84ec8677dbbc52b328fb58
Jagan <jms20041215@gmail.com>
2026-03-01 19:21:18 +0530

    Initial commit

---
+++ longestsubsequence.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class longestsubsequence {
+    public static void main(String[] args){
+   /*Scanner z=new Scanner(System.in);
+        int N=z.nextInt();
+        int[] a=new int[N];
+        int M=0;
+        for(int i=0;i<N;i++){
+            a[i]=z.nextInt();
+        }
+        Map<Integer,Integer> m=new HashMap<>();
+        for(int i:a)
+            m.put(i,1);
+        for(int i=1;i<N;i++)
+        {
+            int n=a[i];
+            for(int j=0;j<i;j++){
+                if(n>a[j])
+                {
+                    if((m.get(a[j]))==(m.get(n)))
+                    {
+                        m.put(n,m.get(n)+1);
+                        M=Math.max(M,m.get(n));
+                    }
+                }
+            }
+        }
+        System.out.println(M);*/
+        Scanner z=new Scanner(System.in);
+        int N=z.nextInt();
+    }    
+}
